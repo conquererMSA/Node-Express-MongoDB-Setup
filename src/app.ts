b989f84afeba1e528f49f1e3857d16643bf42657
asus test <msa-99@outlook.com>
2024-02-17 15:39:25 +0600

    husky setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+app.use(cors());
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+//testing route
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 export default app;
