afcecd8a2df614e8451d9283d2c7db2f2ecae42f
asus test <msa-99@outlook.com>
2024-02-17 15:40:26 +0600

    error

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-app.use(cors());
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-export default app;
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
+})
+export default app
